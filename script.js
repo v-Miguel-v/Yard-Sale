57fda2892057e7b0ac62ea0b67396c06dbed2a69
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-07-01 12:34:12 -0400

    Dropdown Menu
    Se agregó el menú desplegable (dropdown menu) para seleccionar el orden en el que se mostrarán los productos.
    
    Hay un bug extraño que solo me está sucediendo en local en firefox desde desktop: Por alguna razón si subrayo con el mouse los links del menú dropdown, la propiedad style del elemento li subrayado se sobreescribe haciendo que su background-color sea transparent. Es bastante extraño.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const navbar = document.querySelector(".nav-bar");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const dropdownableArea = dropdownMenu.querySelector(".dropdown-menu__dropdownable-area");
+const dropdownMenuActualSelectedLink = dropdownMenu.querySelector(".dropdown-menu__selected-link");
 
 //* Toggle Effect for Nav Links
 navbar.addEventListener("click", toggleActiveEffectForNavLinks);
@@ -16,5 +19,27 @@ function toggleActiveEffectForNavLinks (event) {
 			clickedTarget.classList.add(ACTIVE);
 			clickedTarget.scrollIntoView({ behavior: "smooth", inline: "center", block: "end" });
 		}
+	}
+}
+
+//* Show and Hide Dropdown Menu
+dropdownMenu.addEventListener("click", toggleDropdownMenu);
+function toggleDropdownMenu (event) {
+	event.preventDefault();
+	const clickedTarget = event.target;
+	const LINK = "dropdown-menu__link";
+	const LABEL = "dropdown-menu__label";
+	const ACTIVE = "dropdown-menu__dropdownable-area--active";
+	const isOnTheLabel = (target) => (target.classList.contains(LABEL) || target.parentElement.classList.contains(LABEL));
+	if (isOnTheLabel(clickedTarget)) {
+		dropdownableArea.classList.toggle(ACTIVE);
+	}
+	if (clickedTarget.classList.contains(LINK)) {
+		console.log(clickedTarget);
+		setTimeout(() => {
+			const linkClicked = clickedTarget.innerText;
+			dropdownMenuActualSelectedLink.innerText = linkClicked.replace(" primero", "");
+			dropdownableArea.classList.remove(ACTIVE);
+		}, 80);
 	}
 }
